ac8fa4a8814004d87745a20cf431b6c9d838d0d8
mas <mas154572@gmail.com>
2024-09-05 22:18:31 -0400

    Finsihed review section

--- script.js
+++ script.js
@@ -2,14 +2,19 @@
 
 const menu = document.querySelector("[data-menu]");
 const secondaryNavigation = document.querySelector(
-  "[data-secondary-navigation]"
+  "[data-secondary-navigation"
+);
+const secondaryNavigationList = document.querySelector(
+  "[data-secondary-navigation-list]"
 );
 
 menu.addEventListener("click", function () {
   if (this.getAttribute("aria-expanded") === "false") {
     this.setAttribute("aria-expanded", true);
+    secondaryNavigation.setAttribute("aria-hidden", false);
   } else {
     this.setAttribute("aria-expanded", false);
+    secondaryNavigation.setAttribute("aria-hidden", true);
   }
-  secondaryNavigation.classList.toggle("invisble");
+  secondaryNavigationList.classList.toggle("invisble");
 });
